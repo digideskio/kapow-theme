a5ef60d3e2f7bba7974c655cc1ba21e609ca9b06
Dave Green <hello@davetgreen.me>
2015-09-07 11:57:56 +0100

    Update header.php

--- header.php
+++ header.php
@@ -17,6 +17,9 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<!--[if lt IE 9]>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/header_ie.min.js"></script>
+<![endif]-->
 <?php wp_head(); ?>
 </head>
 
