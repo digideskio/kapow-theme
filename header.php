f21d31b0215478a74fbbedc5821f4d03c83333e9
Dave Green <hello@davetgreen.me>
2015-09-11 22:04:35 +0100

    Escaped get_template_directory_uri

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <!--[if lt IE 9]>
-	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/header_ie.min.js"></script>
+	<script type="text/javascript" src="<?php echo esc_url(get_template_directory_uri()); ?>/js/header_ie.min.js"></script>
 <![endif]-->
 
 <?php wp_head(); ?>
