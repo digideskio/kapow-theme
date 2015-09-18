20886a3c85fafdf5f1c5b0ca3e8480f77e946470
Dave Green <hello@davetgreen.me>
2015-09-18 14:08:22 +0100

    Added better cache busting string when enqueuing customizer JS

--- kapow-theme/inc/customizer.php
+++ kapow-theme/inc/customizer.php
@@ -55,6 +55,6 @@ add_action( 'customize_register', 'my_project_cust_reg_logo' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function my_project_customize_preview_js() {
-	wp_enqueue_script( 'my_project_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'my_project_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), date( 'dmY' ), true );
 }
 add_action( 'customize_preview_init', 'my_project_customize_preview_js' );
